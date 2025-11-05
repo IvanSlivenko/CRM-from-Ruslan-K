55999991ff71e9d424b312a5318498a16fa84be0
IvanSlivenko <ivan8822@ukr.net>
2025-11-05 13:50:54 +0200

    05-11-2025-13-50

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const controller = require('../controllers/auth')
 
 
 //localhost: 5000/api/auth/login
-router.get('/login', (req,res) => {
-    res.status(200).json({
-        login: true
-    })
-})
+router.get('/login', controller.login)
+
+//localhost: 5000/api/auth/register
+router.get('/register', controller.register)
+
 
 module.exports = router
