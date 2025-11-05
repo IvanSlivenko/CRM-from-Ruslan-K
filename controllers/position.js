be3c90f2de11b61866f32e0ab065a3b3ba1c7656
IvanSlivenko <ivan8822@ukr.net>
2025-11-05 20:29:06 +0200

    05-11-2025-20-28

---
+++ controllers/position.js
@@ -0,0 +1,24 @@
+module.exports.getAll = function (req, res) {
+
+}
+
+module.exports.getByCategoryId = function (req, res) {
+
+}
+
+module.exports.create = function (req, res) {
+
+}
+
+module.exports.remove = function (req, res) {
+
+}
+
+
+module.exports.update = function (req, res) {
+
+}
+
+// module.exports.getById = function (req, res) {
+//
+// }
