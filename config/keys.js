523ca969514cd327e03aa4de34216a0d2790e3ca
IvanSlivenko <ivan8822@ukr.net>
2025-11-23 02:52:34 +0200

    23-11-2025-02-52

--- config/keys.js
+++ config/keys.js
@@ -2,3 +2,5 @@ module.exports = {
     // mongoURI: 'mongodb+srv://umanproger:<db_password>@cluster0.jgevz.mongodb.net/?appName=Cluster0'
     mongoURI: 'mongodb+srv://umanproger:iv07092006sl@cluster0.jgevz.mongodb.net/?appName=Cluster0'
 }
+
+
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,49 @@
 const bcrypt = require('bcryptjs')
+
 const User = require('../models/User')
 
-module.exports.login = function(req, res) {
-    res.status(200).json({
-        // login: 'from controller',
-        login: {
-            email: req.body.email,
-            password: req.body.password
+module.exports.login =  async function(req, res) {
+    // var 1
+    // //----------------------------------------------
+    // res.status(200).json({
+    //     // login: 'from controller',
+    //     login: {
+    //         email: req.body.email,
+    //         password: req.body.password
+    //     }
+    //     // login: req.body
+    // })
+    // //----------------------------------------------
+
+    const candidate = await User.findOne({email: req.body.email})
+
+    if (candidate) {
+       // Перевіряємо пароль
+       const passwordResult = await bcrypt.compareSync(req.body.password, candidate.password)
+        if(passwordResult) {
+            //Генеруємо токен, паролі співпали
+            const token = ''
+
+            res.status(200).json({
+                token: token
+            })
+
+        } else {
+            // Паролі не співпали
+            res.status(401).json({
+                message: 'Паролі не співпадають, спробуйте знову'
+            })
         }
-        // login: req.body
-    })
+
+    } else {
+        // користувача не має
+        res.status(404).json({
+            message: `Користувача з емайл ${req.body.email} не знайдено`
+        })
+    }
+
+
+
 }
 
 module.exports.register = async function (req, res) {
