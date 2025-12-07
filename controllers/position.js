37e0c5343d51d381aa280f6e9df34f4dcec51ff0
IvanSlivenko <ivan8822@ukr.net>
2025-12-07 21:47:20 +0200

    07-12-2025-21-47

--- controllers/position.js
+++ controllers/position.js
@@ -38,18 +38,27 @@ module.exports.create = async function (req, res) {
     }
 }
 
-module.exports.remove = function (req, res) {
+module.exports.remove = async function (req, res) {
     try{
-
+        await Position.deleteOne({_id: req.params.id})
+        res.status(200).json({
+            message: 'Позицію видалено'
+        })
     }
     catch(e){
         errorHandler(res, e)
     }
 }
 
-module.exports.update = function (req, res) {
+module.exports.update = async function (req, res) {
     try{
+        const posititon = await Position.findOneAndUpdate(
+            {_id: req.params.id},
+            {$set: req.body},
+            {new: true}
 
+        )
+        res.status(200).json(posititon)
     }
     catch(e){
         errorHandler(res, e)
